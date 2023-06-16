100ac38bb5d180d2a3e42f506d04992d61ad3b73
Vikky779 <olubunmi779@gmail.com>
2023-06-16 07:31:03 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,15 +3,17 @@
  * main - prints the alphabet in lowercase using char function
  *
  * Return: Always 0 (Success)
- */ int main(void)
+ */
+int main(void)
 {
 	char c = 'a'
 
-		while (c <= 'z')
-		{
-			putchar(c);
-			c++;
-		}
+	while (c <= 'z')
+
+	{
+		putchar(c);
+		c++;
+	}
 	putchar('\n');
 	return (0);
 }
