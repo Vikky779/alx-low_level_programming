74ec5ea57b55b32c71caae2294bb52b6ecfb85eb
Vikky779 <olubunmi779@gmail.com>
2023-06-16 09:57:29 -0700

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main- prints all the numbers of base16 in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	int x;
+
+	for (x = o; x < 10; x++)
+	{
+		putchar(x + '0');
+	}
+	for (c = 'a'; c < 'g'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
