01810273957057976d21207db576226442c56e6e
Vikky779 <olubunmi779@gmail.com>
2023-06-16 12:08:03 -0700

    comb4

---
+++ 0x01-variables_if_else_while/101.print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of three digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b, c, d;
+	for (a = 0; a < 1000; a++)
+	{
+		b = a / 1000; /* hundreds */
+		c = (a / 10) % 10; /* tens */
+		d = a % 10; /* singles */
+
+		if (b < c && c < d)
+		{
+			putchar(b + '0');
+			putchar(c + '0');
+			putchar(d + '0');
+
+			if (a < 700)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
