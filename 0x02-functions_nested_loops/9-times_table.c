a37cbf4f6555daba8e80c66f082582721b6dbd0c
Vikky779 <olubunmi779@gmail.com>
2023-06-20 12:07:27 -0700

    table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,9 +13,9 @@ void times_table(void)
 		_putchar('0');
 		_putchar(',');
 		_putchar(' ');
-	for ( c = 1; c <= 9; c++)
+	for (c = 1; c <= 9; c++)
 	{
-		d = ( r * c);
+		d = (r * c);
 	if ((d / 10) > 0)
 	{
 		_putchar((d / 10) + '0');
