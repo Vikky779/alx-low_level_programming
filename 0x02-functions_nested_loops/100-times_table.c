6baf7f27b31928fe26db71bf10634bced5ee6cdf
Vikky779 <olubunmi779@gmail.com>
2023-06-21 06:04:02 -0700

    timestable

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,10 +13,10 @@ void print_times_table(int n)
 		{
 			for (j = 0; j <= n; j++)
 			{
-				k = i * j;
-				if (k == 0 && j == 0)
+				k = j * i;
+				if (j == 0)
 				{
-					_putchar(k + '0');
+					_putchar('k');
 				}
 				else if (k < 10 && j != 0)
 				{
