8717d614befcd6a207f984a11a72b9d991001088
Vikky779 <olubunmi779@gmail.com>
2023-06-23 07:53:18 -0700

    buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,31 +6,24 @@
 int main(void)
 {
 	int a;
+	char f[] = "Fizz";
+	char b[] = "Buzz";
+	char fb[] = "Fizzbuzz";
 
 	for (a = 1; a <= 100; a++)
 	{
-		if (a % 3 == 0 && a % 5 == 0)
-		{
-			printf("Fizzbuzz");
-		}
+		if  (a == 100)
+			printf("%s", b);
+		else if ((a % 3 == 0) && (a % 5 == 0))
+			printf("%s ", fb);
 		else if (a % 3 == 0)
-		{
-			printf("Fizz");
-		}
+			printf("%s ", f);
 		else if (a % 5 == 0)
-		{
-			printf("Buzz");
-		}
+			printf("%s ", b);
 		else
-		{
-			printf("%d", a);
-		}
-		if (a < 100)
-		{
-			printf(" ");
-		}
+			printf("%d ", a);
 	}
-	putchar('\n');
+	printf("\n");
 	return (0);
 }
 
