1d96df23763ddfaefb8e7a43f23f3f64d48fc096
Vikky779 <olubunmi779@gmail.com>
2023-06-23 06:56:25 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - prints from 1 to 100 followed  by a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+
+	a = 1;
+	printf("%d", a);
+	for (a = 2; a <= 100; a++)
+	{
+		if ((a % 3 == 0) && (a % 5 == 0))
+		{
+			printf(" Fizzbuzz");
+		}
+		else if (a % 3 == 0)
+		{
+			printf(" Fizz");
+		}
+		else if (a % 5 == 0)
+		{
+			printf(" Buzz");
+		}
+		else
+		{
+			printf(" %d", a);
+		}
+	}
+	printf("\n");
+	return (0);
+}
+
+
