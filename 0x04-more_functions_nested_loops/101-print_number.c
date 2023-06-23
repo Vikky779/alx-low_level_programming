2cb79a968f2b237606a8adeb92ae5dca6eb7dd46
Vikky779 <olubunmi779@gmail.com>
2023-06-23 10:16:03 -0700

    number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * prime_number - prints an integer
+ * @n: An input integer
+ * Return: Nothing
+ */
+void print_number(int n)
+{
+	unsigned int a, d, count;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		a = n * -1;
+	}
+	else
+	{
+		a = n;
+	}
+
+	d = a;
+	count = 1;
+
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((a / count) % 10) + 48);
+	}
+}
+
