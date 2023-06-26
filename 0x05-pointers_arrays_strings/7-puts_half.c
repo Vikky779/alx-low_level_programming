7e88f4400f537c3ae82aeef082f135bac0e2010d
Vikky779 <olubunmi779@gmail.com>
2023-06-26 14:44:07 -0700

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts_half -  prints half of a string, followed by a new line.
+ * @str: string type
+ * Description: if odd number, print(length - 1)/2
+ */
+void puts_half(char *str)
+{
+	int x;
+	
+	for (x = 0; str[x] != '\0'; x++)
+		;
+	x++;
+	for (x /= 2; str[x] != '\0'; x++)
+	{
+		_putchar(str[x]);
+	}
+	_putchr('\n');
+}
