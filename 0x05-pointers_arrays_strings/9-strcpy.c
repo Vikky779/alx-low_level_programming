54dc1387bdffdee5fb84e78ee063bbba45666e09
Vikky779 <olubunmi779@gmail.com>
2023-06-26 09:27:03 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+i#include "main.h"
+/**
+ * *_strcpy - copies the string pointed to by src to dest
+ * @dest: pointer 1
+ * @src: pointer 2
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *dest[];
+	char *src[];
+
+	strcpy(*dest, *src);
+	printf(strcpy(*dest, *src) : %s\n" , *dest);
+	return (*dest);
+}
