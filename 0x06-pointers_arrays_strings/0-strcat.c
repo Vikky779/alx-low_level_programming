472eb6646639534c8e53bcda85084f9b0453695a
Vikky779 <olubunmi779@gmail.com>
2023-06-29 07:07:16 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,18 +7,23 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
-	j = strlen(dest);
+	int i, j;
 
-	if (!src[i])
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
 	{
-		src[i] = '\0';
-	}
-	else
-	{
-	|	dest[i + j] = src[i];
 		i++;
-		strcat(dest, src);
 	}
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+	dest[i] = '\0';
 	return (dest);
 }
