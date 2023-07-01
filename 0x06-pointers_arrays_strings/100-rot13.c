aaee5a69cf31be836ad2a5969b7c47a841eb4aaf
Vikky779 <olubunmi779@gmail.com>
2023-07-01 05:19:57 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 /**
  * *rot13 - encodes a string using rot13.
- * @s: string
- * Return: Always 0 (Success)
+ * @s: Input string
+ * Return: return string
  */
 char *rot13(char *s)
 {
@@ -12,9 +12,9 @@ char *rot13(char *s)
 	char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char alpha2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-	while (*(s + a) != 0)
+	while (*(s + a) != '\0')
 	{
-		for (a2 = 0; a2 <= 52; a2++)
+		for (a2 = 0; a2 < 52; a2++)
 		{
 			if (*(s + a) == alpha[a2])
 			{
@@ -22,7 +22,7 @@ char *rot13(char *s)
 				break;
 			}
 		}
-				a++;
+		a++;
 	}
-		return (s);
+	return (s);
 }
