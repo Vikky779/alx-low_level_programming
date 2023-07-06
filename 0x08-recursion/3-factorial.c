446e7917c1b51e1f909c6d8241d4b03c46c47558
Vikky779 <olubunmi779@gmail.com>
2023-07-05 18:10:22 -0700

    facto

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,8 +8,8 @@ int factorial(int n)
 {
 	if (n == 0)
 		return (1);
-	else
-		if (n < 0)
+	else if (n < 0)
 		return (-1);
-			factorial(n * factorial(n - 1));
+	else
+		return (n * factorial(n - 1));
 }
