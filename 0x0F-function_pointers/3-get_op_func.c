7fe42395cf15fb2e99a38afc25f9c0f8464b22e1
Vikky779 <olubunmi779@gmail.com>
2023-07-21 06:25:54 -0700

    getop

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,27 @@
+#include "3-calc.h"
+/**
+ * get_op_func - selects correct function
+ * @s: operator passed as argument
+ * Return: Pointer to function corresponding to operator
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t op_s[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	int i;
+
+	while (op_s[i].op)
+	{
+		if (*(op_s[i].op) == *s)
+			return (op_s[i].f);
+		i++;
+	}
+	return (NULL);
+}
