a2452dcb6596e9d629ccf7283e71d72063b6c908
Vikky779 <olubunmi779@gmail.com>
2023-07-21 07:44:26 -0700

    ccalc

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,11 +16,13 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
+
 	if (argv[2][1])
 	{
 		printf("Error\n");
 		exit(99);
 	}
+
 	p = get_op_func(argv[2]);
 
 	if (p == NULL)
@@ -28,8 +30,10 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
+
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
+
 	printf("%d\n", p(a, b));
 	return (0);
 }
