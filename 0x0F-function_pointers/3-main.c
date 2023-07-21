0d418efc8bf3d26399e89b850edebe446d17dd20
Vikky779 <olubunmi779@gmail.com>
2023-07-21 08:13:38 -0700

    main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,32 +8,18 @@
  */
 int main(int argc, char *argv[])
 {
-	int a, b;
-	int (*p)(int, int);
-
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	if (argv[2][1])
+	if (get_op_func(argv[2]))
+		printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+	else
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
-	p = get_op_func(argv[2]);
-
-	if (p == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-
-	printf("%d\n", p(a, b));
 	return (0);
 }
