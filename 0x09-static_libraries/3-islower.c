67d3286883ca359bc73f94157199fc40b298d602
Vikky779 <olubunmi779@gmail.com>
2023-07-25 23:58:47 -0700

    islower file

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islower - Checks lowercase character
+ * @c: Checked character
+ * Return: 1 if lowercase and 0 if otherwise
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c < 'z');
+}
