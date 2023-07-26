1934e4d6da0035fa65c361fce1c8788a669ac939
Vikky779 <olubunmi779@gmail.com>
2023-07-26 00:12:16 -0700

    cmp

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcmp - Compares strings
+ * @s1: Input one
+ * @s2: Input two
+ * Return: string
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int x;
+
+	x = 0;
+	while (s1[x] != '\0' && s2[x] != '\0')
+	{
+		if (s1[x] != s2[x])
+		{
+			return (s1[x] - s2[x]);
+		}
+		x++;
+	}
+	return (0);
+}
