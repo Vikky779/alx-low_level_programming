38c4bf16c48454013ec230bc8b2ec4545345c126
Vikky779 <olubunmi779@gmail.com>
2023-07-25 23:19:26 -0700

    digit

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isdigit - checks for a digit
+ * @c: Input integer
+ * Return: 1 if digit and 0 if otherwise
+ */
+int _isdigit(int c)
+{
+	return (c > '0' && c <= '9');
+}
