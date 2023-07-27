8a851c1ab5c35c216ff6ee54602b7d225f7ebd95
Vikky779 <olubunmi779@gmail.com>
2023-07-27 05:32:36 -0700

    addnode

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * add_node_end - This adds a new node at the end of list
+ * @head: head of the linked list.
+ * @str: string to store in list.
+ * Return: Address of the head.
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *bgn *temp;
+	size_t ch;
+
+	bgn = malloc(sizeof(list_t));
+	if (bgn == NULL)
+		return (NULL);
+
+	bgn->str = strdup(str);
+
+	for (ch = 0; str[ch]; ch++)
+		;
+
+	bgn->len = ch;
+	bgn->next = NULL;
+	temp = *head;
+
+	if (temp == NULL)
+	{
+		*head = bgn;
+	}
+	else
+	{
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = bgn;
+	}
+
+	return (*head);
+}
