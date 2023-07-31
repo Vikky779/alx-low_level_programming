95bc7a20271b929209ab6b1a93bb842d187b08ed
Vikky779 <olubunmi779@gmail.com>
2023-07-31 14:44:15 -0700

    file number eight

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * sum_listint - sum of all the data (n) of list
+ * @head: head of a list.
+ * Return: sum of all the data (n).
+ */
+int sum_listint(listint_t *head)
+{
+	int sum;
+
+	sum = 0;
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
