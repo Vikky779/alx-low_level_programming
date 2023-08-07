5c67c7d5754d90c95fb06d6216f8aacae2d8f823
Vikky779 <olubunmi779@gmail.com>
2023-08-07 14:46:11 -0700

    changes

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -283,7 +283,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
 		exit(98);
 	}
-	p = read(o, header, sizeof(Elf64_Ehdr));
+	p = read(i, header, sizeof(Elf64_Ehdr));
 	if (p == -1)
 	{
 		free(header);
